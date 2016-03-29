c97b3ec9665ea3770640b3f3a8ad0ea3ff93243a
Deepak B N <Deepak B N>
2016-03-29 08:35:28 +0530

    added so many

--- project1.py
+++ project1.py
@@ -1,54 +1,85 @@
-#printing in terminal to check the outcome of 'str' and 'int'
-print('Hello, World!')
-print(100)
+		#printing in terminal to check the outcome of 'str' and 'int'
+		print('Hello, World!')
+		print(100)
 
-#to check what type of values
-type('Hello, World!')
-type(25)
-type(3.2)
-type('10')
-type('3.2')
+		#to check what type of values
+		type('Hello, World!')
+		type(25)
+		type(3.2)
+		type('10')
+		type('3.2')
 
-#veriables
-message='and now for different World'
-pi=3.1415926535897931
-n=17
-cool=pi+n
-print(cool)	
+		#veriables
+		message='and now for different World'
+		pi=3.1415926535897931
+		n=17
+		cool=pi+n
+		print(cool)	
 
-#The type of a variable is the type of the value it refers to
-type(message)
-type(pi)
-type(n)
+		#The type of a variable is the type of the value it refers to
+		type(message)
+		type(pi)
+		type(n)
 
-#The operators +, -, *, / and ** perform addition, subtraction, multiplication, division and exponentiation, as in the following examples:
-#20+32 
-#hour-1 
-#hour*60+minute minute/60 
-#5**2 
-#(5+9)*(15-7)
-#PEMDAS
+		#The operators +, -, *, / and ** perform addition, subtraction, multiplication, division and exponentiation, as in the following examples:
+		#20+32 
+		#hour-1 
+		#hour*60+minute minute/60 
+		#5**2 
+		#(5+9)*(15-7)
+		#PEMDAS
 
-# meal calculater
+		#some time we need to use backlash to skip over single quote
+		'this isn\'t the case for keep over backlash'
 
-meal=45
-tax=12/100
-meal=meal+meal*tax
-tip=15/100
-total=meal+meal*tip
-print (total)
-57.96
+		#String methods
+		len()
+		lower()
+		upper()
+		str()
 
-def clinic():
-    print "You've just entered the clinic!"
-    print "Do you take the door on the left or the right?"
-    answer = raw_input("Type left or right and hit 'Enter'.").lower()
-    if answer == "left" or answer == "l":
-        print "This is the Verbal Abuse Room, you heap of parrot droppings!"
-    elif answer == "right" or answer == "r":
-        print "Of course this is the Argument Room, I've told you that already!"
-    else:
-        print "You didn't pick left or right! Try again."
-        clinic()
+		parrot="Norwegian Blue"
+	print len(parrot)
 
-clinic()
+		parrot = "Norwegian Blue"
+
+	print parrot.lower()
+
+		parrot = "norwegian blue"
+
+	print parrot.upper()
+
+		"""Declare and assign your variable on line 4,
+	then call your method on line 5!"""
+
+	pi=3.14
+	print str(pi)
+
+
+
+		# meal calculater
+
+		meal=45
+		tax=12/100
+		meal=meal+meal*tax
+		tip=15/100
+		total=meal+meal*tip
+		print (total)
+		57.96
+
+
+		control following
+		
+		def clinic():
+		    print "You've just entered the clinic!"
+		    print "Do you take the door on the left or the right?"
+		    answer = raw_input("Type left or right and hit 'Enter'.").lower()
+		    if answer == "left" or answer == "l":
+		        print "This is the Verbal Abuse Room, you heap of parrot droppings!"
+		    elif answer == "right" or answer == "r":
+		        print "Of course this is the Argument Room, I've told you that already!"
+		    else:
+		        print "You didn't pick left or right! Try again."
+		        clinic()
+
+		clinic()
