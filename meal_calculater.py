cdc9da75312087276ebbeee5da1bae9b5cf86640
Deepak B N <Deepak B N>
2016-04-02 22:48:31 +0530

    future requirements add

--- meal_calculater.py
+++ meal_calculater.py
@@ -1,5 +1,7 @@
 # meal price calculater
 
+#stage 1
+
 meal=input("what is the price of the meal?")
 meal=int(meal)
 tax=input("what is the percent of tax on meal?(only number)")
@@ -8,6 +10,8 @@ tax=tax/100
 meal_plus_tax=meal+meal*tax
 
 print ("price of meal plus tax Rs "+ str(meal_plus_tax))
+
+#stage 2
 
 dude=input("did you pay tip to the waiter?")
 
@@ -23,3 +27,9 @@ if  dude=="yes"or dude=="y":
 else :
 	
 	print ("price of your meal is Rs "+str(meal_plus_tax))					
+
+#stage 3
+
+"""1. now i want to select different formulas for calculation
+   2. add items in grocery store and calculating tax on them
+	  at the end"""
