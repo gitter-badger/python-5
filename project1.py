a85a90d630a0593a4d79f1f29b09f8bba6c90b02
Deepak B N <Deepak B N>
2016-04-02 23:29:21 +0530

    added bit info about if elif and else

--- project1.py
+++ project1.py
@@ -79,20 +79,44 @@
 
 
 		control following
-		
-		def clinic():
-		    print "You've just entered the clinic!"
-		    print "Do you take the door on the left or the right?"
-		    answer = raw_input("Type left or right and hit 'Enter'.").lower()
-		    if answer == "left" or answer == "l":
-		        print "This is the Verbal Abuse Room, you heap of parrot droppings!"
-		    elif answer == "right" or answer == "r":
-		        print "Of course this is the Argument Room, I've told you that already!"
-		    else:
-		        print "You didn't pick left or right! Try again."
-		        clinic()
 
-		clinic()
+def using_control_once():
+    if 5>=5:
+        return "Success #1"
+
+def using_control_again():
+    if 10>9:
+        return "Success #2"
+
+print using_control_once()
+print using_control_again()		
+		
+def clinic():
+	print "You've just entered the clinic!"
+	print "Do you take the door on the left or the right?"
+	answer = raw_input("Type left or right and hit 'Enter'.").lower()
+	if answer == "left" or answer == "l":
+	    print "This is the Verbal Abuse Room, you heap of parrot droppings!"
+	elif answer == "right" or answer == "r":
+	    print "Of course this is the Argument Room, I've told you that already!"
+	else:
+	    print "You didn't pick left or right! Try again."
+	    clinic()
+
+	clinic()
+
+# Make sure that the_flying_circus() returns True
+def the_flying_circus():
+    if (10>=9) or (9>=10):    # Start coding here!
+        # Don't forget to indent
+        # the code inside this block!
+        return True
+    elif (10>=9) and (9>=10):
+        # Keep going here.
+        # You'll want to add the else statement, too!
+        return False
+    
+    print the_flying_circus()
 
 
 name = raw_input("What is your name?")
