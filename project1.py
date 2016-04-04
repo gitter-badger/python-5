eb975e3ee6c49abf4c30cd23c9c87647b33a5699
Deepak B N <Deepak B N>
2016-04-04 19:15:51 +0530

    added some code

--- project1.py
+++ project1.py
@@ -168,3 +168,46 @@ bool_four = False
 
 # Set this to True if 99 != (98 + 1) or to False otherwise.
 bool_five = False
+
+
+#Pig Latin Translater
+
+print 'Welcome to the Pig Latin Translator!'
+
+# Start coding here!
+original=raw_input("Enter a word:")
+if len(original)>0 and original.isalpha():
+    print (original)
+else:
+    print "empty"
+
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+    word=original.lower()
+    first=word[0]
+    new_word=word+first+pyg
+    new_word=new_word[1:len(new_word)]
+    print new_word
+else:
+    print 'empty'
+    
+#this is all about functions
+
+def tax(bill):
+    """Adds 8% tax to a restaurant bill."""
+    bill *= 1.08
+    print "With tax: %f" % bill
+    return bill
+
+def tip(bill):
+    """Adds 15% tip to a restaurant bill."""
+    bill *= 1.15
+    print "With tip: %f" % bill
+    return bill
+    
+meal_cost = 100
+meal_with_tax = tax(meal_cost)
+meal_with_tip = tip(meal_with_tax)
